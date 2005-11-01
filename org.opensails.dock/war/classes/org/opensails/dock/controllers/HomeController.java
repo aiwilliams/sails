cf6d1b454dc5003d132ea03a2a155e02e9443a7f
Adam Williams <adam@thewilliams.ws>
2005-11-01 15:49:16 +0000

    Added some uses of urlfor, with different parameters.

--- org.opensails.dock/war/classes/org/opensails/dock/controllers/HomeController.java
+++ org.opensails.dock/war/classes/org/opensails/dock/controllers/HomeController.java
@@ -1,13 +1,24 @@
 package org.opensails.dock.controllers;
 
+import org.opensails.dock.model.User;
 import org.opensails.sails.controller.oem.BaseController;
 
 public class HomeController extends BaseController {
-    public void index() {
-        index("World");
-    }
-    
-    public void index(String who) {
-        expose("who", who);
-    }
+	public void helloName(String who) {
+		expose("who", who);
+		renderTemplate("index");
+	}
+
+	public void helloUser(User who) {
+		expose("who", who.getFirstName());
+		renderTemplate("index");
+	}
+
+	public void index() {
+		index("World");
+	}
+
+	public void index(String who) {
+		expose("who", who);
+	}
 }
--- org.opensails.dock/war/classes/org/opensails/dock/model/User.java
+++ org.opensails.dock/war/classes/org/opensails/dock/model/User.java
@@ -5,6 +5,10 @@ import org.opensails.sails.persist.IIdentifiable;
 public class User implements IIdentifiable {
 	protected String firstName;
 
+	public User() {
+		firstName = "Initial Value";
+	}
+
 	public String getFirstName() {
 		return firstName;
 	}
