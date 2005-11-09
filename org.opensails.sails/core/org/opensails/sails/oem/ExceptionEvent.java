1d3f01b7eef42be183698270efe61db9597c9850
Adam Williams <adam@thewilliams.ws>
2005-11-09 21:44:57 +0000

    Added error status on exceptions

--- org.opensails.sails/core/org/opensails/sails/oem/ExceptionEvent.java
+++ org.opensails.sails/core/org/opensails/sails/oem/ExceptionEvent.java
@@ -1,50 +1,53 @@
 package org.opensails.sails.oem;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.opensails.sails.ISailsEvent;
 import org.opensails.sails.controller.IActionResult;
 import org.opensails.sails.controller.oem.Controller;
 
 public class ExceptionEvent extends AbstractEvent {
-    public static final String CONTROLLER_NAME = "error";
-    
-    protected final Throwable exception;
-    protected ISailsEvent originatingEvent;
+	public static final String CONTROLLER_NAME = "error";
 
-    public ExceptionEvent(ISailsEvent event, Throwable t) {
-        super(event.getApplication(), event.getRequest(), event.getResponse());
-        this.originatingEvent = event;
-        this.exception = t;
-        setContainer(event.getContainer());
-    }
+	protected final Throwable exception;
+	protected ISailsEvent originatingEvent;
 
-    @Override
-    public void beginDispatch() {}
+	public ExceptionEvent(ISailsEvent event, Throwable t) {
+		super(event.getApplication(), event.getRequest(), event.getResponse());
+		this.originatingEvent = event;
+		this.exception = t;
+		setContainer(event.getContainer());
+		getResponse().setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+	}
 
-    @Override
-    public void endDispatch() {}
+	@Override
+	public void beginDispatch() {}
 
-    public Throwable getException() {
-        return exception;
-    }
+	@Override
+	public void endDispatch() {}
 
-    public ISailsEvent getOriginatingEvent() {
-        return originatingEvent;
-    }
+	public Throwable getException() {
+		return exception;
+	}
 
-    @Override
-    public IActionResult visit(Controller controller) {
-        return controller.process(this);
-    }
+	public ISailsEvent getOriginatingEvent() {
+		return originatingEvent;
+	}
 
-    @Override
-    protected void containerSet() {
-        container.register(this);
-    }
+	@Override
+	public IActionResult visit(Controller controller) {
+		return controller.process(this);
+	}
 
-    @Override
-    protected void initialize() {
-        super.initialize();
-        url.setAction("exception");
-        url.setController("error");
-    }
+	@Override
+	protected void containerSet() {
+		container.register(this);
+	}
+
+	@Override
+	protected void initialize() {
+		super.initialize();
+		url.setAction("exception");
+		url.setController("error");
+	}
 }
--- org.opensails.sails/test/unit/org/opensails/sails/oem/ExceptionEventTest.java
+++ org.opensails.sails/test/unit/org/opensails/sails/oem/ExceptionEventTest.java
@@ -1,23 +1,28 @@
 package org.opensails.sails.oem;
 
+import javax.servlet.http.HttpServletResponse;
+
 import junit.framework.TestCase;
 
 import org.opensails.sails.ISailsEvent;
+import org.opensails.sails.tester.servletapi.ShamHttpServletResponse;
 
 public class ExceptionEventTest extends TestCase {
-    public void testInitialize() throws Exception {
-        ISailsEvent originatingEvent = SailsEventFixture.actionGet("controller", "action");
-        ExceptionEvent event = new ExceptionEvent(originatingEvent, new RuntimeException());
-        assertEquals("error", event.getControllerName());
-        assertEquals("exception", event.getActionName());
-        assertNotNull(event.getApplication());
-        assertSame(originatingEvent.getApplication(), event.getApplication());
-    }
+	public void testInitialize() throws Exception {
+		ISailsEvent originatingEvent = SailsEventFixture.actionGet("controller", "action");
+		ExceptionEvent event = new ExceptionEvent(originatingEvent, new RuntimeException());
+		assertEquals("error", event.getControllerName());
+		assertEquals("exception", event.getActionName());
+		assertNotNull(event.getApplication());
+		assertSame(originatingEvent.getApplication(), event.getApplication());
 
-    public void testSetContainer() {
-        ExceptionEvent event = SailsEventFixture.actionException("controller", "action");
-        assertSame(event.getOriginatingEvent(), event.getContainer().instance(ISailsEvent.class));
-        assertSame(event.getOriginatingEvent(), event.getContainer().instance(event.getOriginatingEvent().getClass()));
-        assertSame(event, event.getContainer().instance(ExceptionEvent.class));
-    }
+		assertEquals(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, ((ShamHttpServletResponse) originatingEvent.getResponse()).getStatus());
+	}
+
+	public void testSetContainer() {
+		ExceptionEvent event = SailsEventFixture.actionException("controller", "action");
+		assertSame(event.getOriginatingEvent(), event.getContainer().instance(ISailsEvent.class));
+		assertSame(event.getOriginatingEvent(), event.getContainer().instance(event.getOriginatingEvent().getClass()));
+		assertSame(event, event.getContainer().instance(ExceptionEvent.class));
+	}
 }
