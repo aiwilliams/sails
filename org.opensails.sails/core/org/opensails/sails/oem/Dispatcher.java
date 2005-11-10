fa9d5edb8e83c5e301fb88f08499909a0cbd1bfa
Austin Taylor <austin.taylor@gmail.com>
2005-11-10 22:48:25 +0000

    Added logging for a few things.

--- org.opensails.sails/core/org/opensails/sails/oem/Dispatcher.java
+++ org.opensails.sails/core/org/opensails/sails/oem/Dispatcher.java
@@ -1,79 +1,94 @@
 package org.opensails.sails.oem;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.opensails.rigging.ScopedContainer;
 import org.opensails.sails.ApplicationScope;
 import org.opensails.sails.IActionResultProcessor;
 import org.opensails.sails.IActionResultProcessorResolver;
 import org.opensails.sails.ISailsApplication;
 import org.opensails.sails.ISailsEventConfigurator;
+import org.opensails.sails.controller.IAction;
 import org.opensails.sails.controller.IActionResult;
 import org.opensails.sails.controller.oem.Controller;
 import org.opensails.sails.controller.oem.IControllerResolver;
 
 public class Dispatcher {
-    protected final ISailsApplication application;
-    protected final IControllerResolver controllerResolver;
-    protected final ISailsEventConfigurator eventConfigurator;
-    protected final IActionResultProcessorResolver processorResolver;
+	protected static final Log DISPATCHER_LOG = LogFactory.getLog(Dispatcher.class);
+	protected static final Log ACTION_LOG = LogFactory.getLog(IAction.class);
+	protected static final Log RESULT_LOG = LogFactory.getLog(IActionResultProcessor.class);
 
-    public Dispatcher(ISailsApplication application, ISailsEventConfigurator configurator, IControllerResolver controllerResolver, IActionResultProcessorResolver processorResolver) {
-        this.application = application;
-        this.eventConfigurator = configurator;
-        this.controllerResolver = controllerResolver;
-        this.processorResolver = processorResolver;
-    }
+	protected final ISailsApplication application;
+	protected final IControllerResolver controllerResolver;
+	protected final ISailsEventConfigurator eventConfigurator;
+	protected final IActionResultProcessorResolver processorResolver;
 
-    public void dispatch(ExceptionEvent event) {
-        event.beginDispatch();
-        process(event);
-        event.endDispatch();
-    }
+	public Dispatcher(ISailsApplication application, ISailsEventConfigurator configurator, IControllerResolver controllerResolver, IActionResultProcessorResolver processorResolver) {
+		this.application = application;
+		this.eventConfigurator = configurator;
+		this.controllerResolver = controllerResolver;
+		this.processorResolver = processorResolver;
+	}
 
-    public void dispatch(GetEvent event) {
-        dispatch((ILifecycleEvent) event);
-    }
+	public void dispatch(ExceptionEvent event) {
+		event.beginDispatch();
+		process(event);
+		event.endDispatch();
+	}
 
-    public void dispatch(ILifecycleEvent event) {
-        beginDispatch(event);
-        try {
-            process(event);
-        } catch (Throwable t) {
-            dispatch(new ExceptionEvent(event, t));
-        }
-        endDispatch(event);
-    }
+	public void dispatch(GetEvent event) {
+		dispatch((ILifecycleEvent) event);
+	}
 
-    public void dispatch(PostEvent event) {
-        dispatch((ILifecycleEvent) event);
-    }
+	public void dispatch(ILifecycleEvent event) {
+		DISPATCHER_LOG.debug(event);
+		beginDispatch(event);
+		try {
+			process(event);
+		} catch (Throwable t) {
+			dispatch(new ExceptionEvent(event, t));
+		}
+		endDispatch(event);
+		DISPATCHER_LOG.debug(event);
+	}
 
-    protected void beginDispatch(ILifecycleEvent event) {
-        installContainer(event);
-        event.beginDispatch();
-    }
+	public void dispatch(PostEvent event) {
+		dispatch((ILifecycleEvent) event);
+	}
 
-    protected void destroyContainer(ILifecycleEvent event) {
-        event.getContainer().getParent().removeChild(event.getContainer());
-    }
+	protected void beginDispatch(ILifecycleEvent event) {
+		installContainer(event);
+		event.beginDispatch();
+	}
 
-    protected void endDispatch(ILifecycleEvent event) {
-        event.endDispatch();
-        destroyContainer(event);
-    }
+	protected void destroyContainer(ILifecycleEvent event) {
+		event.getContainer().getParent().removeChild(event.getContainer());
+	}
 
-    protected void installContainer(ILifecycleEvent event) {
-        ScopedContainer applicationContainer = application.getContainer();
-        ScopedContainer eventContainer = applicationContainer.makeChild(ApplicationScope.REQUEST);
-        event.setContainer(eventContainer);
-        eventConfigurator.configure(event, eventContainer);
-    }
+	protected void endDispatch(ILifecycleEvent event) {
+		event.endDispatch();
+		destroyContainer(event);
+	}
 
-    @SuppressWarnings("unchecked")
-    protected void process(ILifecycleEvent event) {
-        Controller controller = controllerResolver.resolve(event.getControllerName());
-        // delegate to event so that is calls most specific process
-        IActionResult result = event.visit(controller);
-        IActionResultProcessor processor = processorResolver.resolve(result);
-        processor.process(result);
-    }
+	protected void installContainer(ILifecycleEvent event) {
+		ScopedContainer applicationContainer = application.getContainer();
+		ScopedContainer eventContainer = applicationContainer.makeChild(ApplicationScope.REQUEST);
+		event.setContainer(eventContainer);
+		eventConfigurator.configure(event, eventContainer);
+	}
+
+	@SuppressWarnings("unchecked")
+	protected void process(ILifecycleEvent event) {
+		Controller controller = controllerResolver.resolve(event.getControllerName());
+
+		ACTION_LOG.debug(event);
+		// delegate to event so that is calls most specific process
+		IActionResult result = event.visit(controller);
+		ACTION_LOG.debug(event);
+
+		RESULT_LOG.debug(event);
+		IActionResultProcessor processor = processorResolver.resolve(result);
+		processor.process(result);
+		RESULT_LOG.debug(event);
+	}
 }
