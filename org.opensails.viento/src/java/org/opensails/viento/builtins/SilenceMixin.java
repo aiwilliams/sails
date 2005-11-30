56bf3ca03b3a46abb830c74bc1fc8db512958123
Austin Taylor <austin.taylor@gmail.com>
2005-11-30 19:07:28 +0000

    faster $![[]]

--- org.opensails.viento/src/java/org/opensails/viento/builtins/SilenceMixin.java
+++ org.opensails.viento/src/java/org/opensails/viento/builtins/SilenceMixin.java
@@ -1,9 +1,10 @@
 package org.opensails.viento.builtins;
 
 import org.opensails.viento.Block;
-import org.opensails.viento.DefaultExceptionHandler;
+import org.opensails.viento.ExceptionHandler;
 import org.opensails.viento.Name;
-import org.opensails.viento.ResolutionFailedException;
+import org.opensails.viento.TargetedMethodKey;
+import org.opensails.viento.TopLevelMethodKey;
 
 public class SilenceMixin {
 	@Name("!")
@@ -11,9 +12,10 @@ public class SilenceMixin {
 		return new Silence(block);
 	}
 	
-	public class Silence {
+	public class Silence implements ExceptionHandler {
 		protected Block block;
 		protected Block notFound;
+		protected boolean somethingFailed = false;
 
 		public Silence(Block block) {
 			this.block = block;
@@ -27,14 +29,34 @@ public class SilenceMixin {
 		
 		@Override
 		public String toString() {
-			block.getBinding().setExceptionHandler(new DefaultExceptionHandler());
-			try {
-				return block.evaluate();
-			} catch (ResolutionFailedException e) {
-				if (notFound != null)
-					return notFound.evaluate();
-				return "";
-			}
+			block.getBinding().setExceptionHandler(this);
+			String result = block.evaluate();
+			if (!somethingFailed)
+				return result;
+			if (notFound != null)
+				return notFound.evaluate();
+			return "";
+		}
+
+		private Object somethingFailed() {
+			somethingFailed = true;
+			return "";
+		}
+
+		public Object resolutionFailed(TargetedMethodKey key, Object target, Object[] args) {
+			return somethingFailed();
+		}
+
+		public Object resolutionFailed(TopLevelMethodKey key, Object[] args) {
+			return somethingFailed();
+		}
+
+		public Object resolutionFailed(Throwable exception, TopLevelMethodKey key, Object[] args) {
+			return somethingFailed();
+		}
+
+		public Object resolutionFailed(Throwable exception, TargetedMethodKey key, Object target, Object[] args) {
+			return somethingFailed();
 		}
 	}
 }
