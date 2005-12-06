af9ebf9b4a4f865e5e6a9c26283a743f4c9772ca
Austin Taylor <austin.taylor@gmail.com>
2005-12-06 22:54:05 +0000

    more accessable escaping

--- org.opensails.viento/src/java/org/opensails/viento/builtins/EscapeMixin.java
+++ org.opensails.viento/src/java/org/opensails/viento/builtins/EscapeMixin.java
@@ -19,6 +19,12 @@ public class EscapeMixin {
 	}
 	
 	public String h(Object object) {
+		return EscapeMixin.escapeHtml(object);
+	}
+	
+	public static String escapeHtml(Object object) {
+		if (object == null)
+			return null;
 		return object.toString().replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;");
 	}
 }
