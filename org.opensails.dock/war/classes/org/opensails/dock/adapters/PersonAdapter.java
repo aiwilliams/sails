ac22082477c60d2562adb929a789818aedc3bf74
Austin Taylor <austin.taylor@gmail.com>
2005-12-14 19:46:54 +0000

    Serious IAdapter Refactoring

--- org.opensails.dock/war/classes/org/opensails/dock/adapters/PersonAdapter.java
+++ org.opensails.dock/war/classes/org/opensails/dock/adapters/PersonAdapter.java
@@ -1,19 +1,15 @@
 package org.opensails.dock.adapters;
 
 import org.opensails.dock.model.Person;
+import org.opensails.sails.adapter.AbstractAdapter;
 import org.opensails.sails.adapter.AdaptationException;
-import org.opensails.sails.adapter.IAdapter;
 
-public class PersonAdapter implements IAdapter {
-	public Class[] getSupportedTypes() {
-		return new Class[] { Person.class };
-	}
-
-	public Object forModel(Class modelType, Object fromWeb) throws AdaptationException {
+public class PersonAdapter extends AbstractAdapter<Person, String> {
+	public Person forModel(Class<? extends Person> modelType, String fromWeb) throws AdaptationException {
 		return new Person();
 	}
 
-	public Object forWeb(Class modelType, Object fromModel) throws AdaptationException {
-		return ((Person) fromModel).getName();
+	public String forWeb(Class<? extends Person> modelType, Person fromModel) throws AdaptationException {
+		return fromModel.getName();
 	}
 }
