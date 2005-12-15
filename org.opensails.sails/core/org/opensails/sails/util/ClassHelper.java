704153ffd56250c2f90b90e00e4765b7b8a3085f
Adam Williams <adam@thewilliams.ws>
2005-12-15 19:27:20 +0000

    Added fieldsAnnotated

--- org.opensails.sails/core/org/opensails/sails/util/ClassHelper.java
+++ org.opensails.sails/core/org/opensails/sails/util/ClassHelper.java
@@ -1,11 +1,11 @@
 package org.opensails.sails.util;
 
-import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
+import java.lang.annotation.*;
+import java.lang.reflect.*;
+import java.util.*;
 
-import org.apache.commons.lang.ClassUtils;
-import org.opensails.sails.SailsException;
+import org.apache.commons.lang.*;
+import org.opensails.sails.*;
 
 /**
  * YAGNI and YARGNI, all in one class.
@@ -25,6 +25,14 @@ public class ClassHelper {
 		} catch (Throwable t) {
 			throw new RuntimeException(String.format("Could not find a field named %s on %s", name, clazz), t);
 		}
+	}
+
+	public static Field[] fieldsAnnotated(Class<?> clazz, Class<? extends Annotation> annotation) {
+		Field[] declaredFields = clazz.getDeclaredFields();
+		List<Field> annotatedFields = new ArrayList<Field>(declaredFields.length);
+		for (Field field : declaredFields)
+			if (field.isAnnotationPresent(annotation)) annotatedFields.add(field);
+		return annotatedFields.toArray(new Field[annotatedFields.size()]);
 	}
 
 	public static Field[] fieldsNamed(Class clazz, String... names) {
