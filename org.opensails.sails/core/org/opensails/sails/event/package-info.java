645e4e03b69597d2cd2973133172fd6913452a59
Austin Taylor <austin.taylor@gmail.com>
2005-12-16 13:49:58 +0000

    Added first ever package-info.java javadoc thing!

---
+++ org.opensails.sails/core/org/opensails/sails/event/package-info.java
@@ -0,0 +1,12 @@
+package org.opensails.sails.event;
+
+/**
+ * Defines the interfaces and classes that comprise the event model of a Sails
+ * application.
+ * <p>
+ * Sails is essentially a Servlet application. As such, events are typically
+ * generated through HTTP GET and POST events. There are other types of events
+ * as well, though many are not yet supported by Sails.
+ * 
+ * @since 0.3
+ */
