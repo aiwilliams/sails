a41310263dbfae2d0d698a7f1f0c20d52868bba7
Adam Williams <adam@thewilliams.ws>
2005-12-17 05:33:02 +0000

    Massive package refactoring

--- org.opensails.dock/war/classes/org/opensails/dock/controllers/PartialsController.java
+++ org.opensails.dock/war/classes/org/opensails/dock/controllers/PartialsController.java
@@ -1,7 +1,7 @@
 package org.opensails.dock.controllers;
 
+import org.opensails.sails.action.oem.PartialActionResult;
 import org.opensails.sails.controller.oem.BaseController;
-import org.opensails.sails.controller.oem.PartialActionResult;
 
 public class PartialsController extends BaseController {
 	public PartialActionResult renderPartial() {
