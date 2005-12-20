639bdbc69112fa79aaa8da936b1126753328b28d
Austin Taylor <austin.taylor@gmail.com>
2005-12-20 20:22:50 +0000

    Made IfMixin support any object as body of if

--- org.opensails.viento/src/java/org/opensails/viento/builtins/IfMixin.java
+++ org.opensails.viento/src/java/org/opensails/viento/builtins/IfMixin.java
@@ -1,34 +1,39 @@
 package org.opensails.viento.builtins;
 
-import org.opensails.viento.Block;
 import org.opensails.viento.Name;
 
 public class IfMixin {
+	/**
+	 * @param condition
+	 * @param object
+	 *            to output, usually a Block
+	 * @return an If
+	 */
 	@Name("if")
-	public If create(Object condition, Block block) {
-		return new If().condition(condition, block);
+	public If create(Object condition, Object body) {
+		return new If().condition(condition, body);
 	}
-	
+
 	public class If {
-		Block blockToRender;
-		
-		@Override
-		public String toString() {
-			return blockToRender == null ? "" : blockToRender.evaluate();
-		}
+		Object bodyToEvaluate;
 
 		@Name("elseif")
-		public If condition(Object condition, Block block) {
-			if (condition != null && !condition.equals(Boolean.FALSE) && blockToRender == null)
-				blockToRender = block;
+		public If condition(Object condition, Object body) {
+			if (condition != null && !condition.equals(Boolean.FALSE) && bodyToEvaluate == null)
+				bodyToEvaluate = body;
 			return this;
 		}
-		
+
 		@Name("else")
-		public If noCondition(Block block) {
-			if (blockToRender == null)
-				blockToRender = block;
+		public If noCondition(Object body) {
+			if (bodyToEvaluate == null)
+				bodyToEvaluate = body;
 			return this;
+		}
+
+		@Override
+		public String toString() {
+			return bodyToEvaluate == null ? "" : bodyToEvaluate.toString();
 		}
 	}
 }
