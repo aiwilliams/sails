505a15bbe17e9153a38348bb1f4cea6f9ef9c4e4
Austin Taylor <austin.taylor@gmail.com>
2005-12-20 21:32:55 +0000

    Outputting content length in header.

--- org.opensails.sails/builtin/org/opensails/sails/processors/StringActionResultProcessor.java
+++ org.opensails.sails/builtin/org/opensails/sails/processors/StringActionResultProcessor.java
@@ -2,9 +2,13 @@ package org.opensails.sails.processors;
 
 import org.opensails.sails.action.IActionResultProcessor;
 import org.opensails.sails.action.oem.StringActionResult;
+import org.opensails.sails.event.ISailsEvent;
 
 public class StringActionResultProcessor implements IActionResultProcessor<StringActionResult> {
 	public void process(StringActionResult result) {
-		result.getEvent().write(result.getContent());
+		ISailsEvent event = result.getEvent();
+		String content = result.getContent();
+		event.getResponse().setContentLength(content.length());
+		event.write(content);
 	}
 }
