8220fc4bda28fb835dfc0aff3cc50c7869eef2d2
Adam Williams <adam@thewilliams.ws>
2006-01-09 05:20:04 +0000

    Feature test for ticket:64

--- org.opensails.sails/test/functional/org/opensails/functional/controllers/EventTestSubclassController.java
+++ org.opensails.sails/test/functional/org/opensails/functional/controllers/EventTestSubclassController.java
@@ -1,5 +1,11 @@
 package org.opensails.functional.controllers;
 
-public class EventTestSubclassController extends EventTestController {
+import junit.framework.Assert;
 
+import org.opensails.sails.event.ISailsEvent;
+
+public class EventTestSubclassController extends EventTestController {
+	public EventTestSubclassController(ISailsEvent event) {
+		Assert.assertNotNull("You can access the event in the constructor if you declare the dependency. Anything else in the container, too", event);
+	}
 }
