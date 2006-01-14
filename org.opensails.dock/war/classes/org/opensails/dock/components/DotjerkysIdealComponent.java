f940fdf61556531d93adc0950ed81d7840e3d284
Austin Taylor <austin.taylor@gmail.com>
2006-01-14 02:09:15 +0000

    slightly more conjecture

--- org.opensails.dock/war/classes/org/opensails/dock/components/DotjerkysIdealComponent.java
+++ org.opensails.dock/war/classes/org/opensails/dock/components/DotjerkysIdealComponent.java
@@ -1,12 +1,22 @@
 package org.opensails.dock.components;
 
+import org.opensails.sails.persist.IObjectPersister;
+
 public class DotjerkysIdealComponent /*extends BaseComponent*/ {
+	private final IObjectPersister persister;
+	
 	String requiredProperty;
-	/*@Remembered*/ String anotherProperty = "default";
+//	@Remembered
+	String anotherProperty = "default";
 	Object complex;
 	
-	public DotjerkysIdealComponent(String id, String requiredProperty) {
-//		super(id);
+	public DotjerkysIdealComponent(IObjectPersister persister) {
+		this.persister = persister;
+	}
+	
+//	@Override
+	public void create(String id, String requiredProperty) {
+//		super.create(id);
 		this.requiredProperty = requiredProperty;
 	}
 	
@@ -16,11 +26,13 @@ public class DotjerkysIdealComponent /*extends BaseComponent*/ {
 //		expose("complex", someTransform(complex));
 	}
 	
-	/*@Callback*/ void doSomething(String somethingInteresting) {
+//	@Callback
+	void doSomething(String somethingInteresting) {
 //		assert id != null;
 		assert anotherProperty != null;
 		assert requiredProperty == null;
 		assert somethingInteresting.equals("really really interesting");
+		persister.commit();
 //		renderString("very good");
 	}
 }
