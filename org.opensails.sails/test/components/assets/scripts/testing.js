1ca48b11a3ec620fd2f10d6f53ea7d2a67f51cbe
Austin Taylor <austin.taylor@gmail.com>
2006-02-02 21:11:04 +0000

    Fleshing out better component script and style dependency requirement.

---
+++ org.opensails.sails/test/components/assets/scripts/testing.js
@@ -0,0 +1,5 @@
+/*
+ * The fact that this file is in the scripts directory is irrelevent. You can require
+ * whatever you like, relative to the components directory. The RequireMixin is 'mounted'
+ * on the component directory.
+ */
--- org.opensails.sails/test/functional/org/opensails/functional/component/BasicComponentTests.java
+++ org.opensails.sails/test/functional/org/opensails/functional/component/BasicComponentTests.java
@@ -1,13 +1,23 @@
 package org.opensails.functional.component;
 
-import junit.framework.*;
+import junit.framework.TestCase;
 
-import org.apache.commons.lang.*;
-import org.opensails.functional.*;
-import org.opensails.functional.components.*;
-import org.opensails.sails.tester.*;
+import org.apache.commons.lang.ArrayUtils;
+import org.opensails.functional.SailsFunctionalTester;
+import org.opensails.functional.components.BasicComponent;
+import org.opensails.sails.tester.Page;
 
 public class BasicComponentTests extends TestCase {
+	public void testAssets() throws Exception {
+		SailsFunctionalTester t = new SailsFunctionalTester();
+		Page page = t.get("componentTest", "assets", ArrayUtils.EMPTY_STRING_ARRAY);
+		/**
+		 * The required scripts come before the implicit scripts as the implicit
+		 * scripts use the required scripts.
+		 */
+		page.assertMatches("Expected failure until implemented", "<script.*?src=\".*?components/assets/scripts/testing.js\".*/<script.*?src=\".*?components/assets/script.js\"");
+	}
+
 	public void testBasic() throws Exception {
 		SailsFunctionalTester t = new SailsFunctionalTester();
 		Page page = t.get("componentTest", "basic", ArrayUtils.EMPTY_STRING_ARRAY);
