e255643173b6abf0976410e143cc4b54f70cf9d2
Michael Hale <mikehale@gmail.com>
2006-03-10 08:07:09 +0000

    use new api

--- org.opensails.prevayler/src/main/java/org/opensails/prevayler/PrevaylerApplicationConfigurator.java
+++ org.opensails.prevayler/src/main/java/org/opensails/prevayler/PrevaylerApplicationConfigurator.java
@@ -1,8 +1,8 @@
 package org.opensails.prevayler;
 
-import org.opensails.rigging.ScopedContainer;
+import org.opensails.sails.ApplicationContainer;
 import org.opensails.sails.IConfigurableSailsApplication;
-import org.opensails.sails.RequestContainer;
+import org.opensails.sails.IEventContextContainer;
 import org.opensails.sails.event.ISailsEvent;
 import org.opensails.sails.oem.BaseConfigurator;
 import org.opensails.sails.persist.IObjectPersister;
@@ -13,13 +13,13 @@ public abstract class PrevaylerApplicationConfigurator extends BaseConfigurator 
 	// TODO why do I need to implement this method in addition to installObjectPersister()? I don't understand it's usefullness. This should probably be tested.
 	// For Database based persistance IObjectPersisters are registered at the Request scope to keep transactions as short lived as possible.
 	@Override
-	public void configure(ISailsEvent event, RequestContainer eventContainer) {
+	public void configure(ISailsEvent event, IEventContextContainer eventContainer) {
 		super.configure(event, eventContainer);
 		eventContainer.register(IObjectPersister.class, PrevaylerPersister.class);
 	}
 
 	@Override
-	protected void installObjectPersister(IConfigurableSailsApplication application, ScopedContainer container) {
+	protected void installObjectPersister(IConfigurableSailsApplication application, ApplicationContainer container) {
 		super.installObjectPersister(application, container);
 		container.register(Prevayler.class, getPrevayler());
 		container.register(IObjectPersister.class, PrevaylerPersister.class);
@@ -29,10 +29,10 @@ public abstract class PrevaylerApplicationConfigurator extends BaseConfigurator 
 	 * Example implementation:
 	 * 
 	 * IdentifiableObjectPrevalentSystem prevalantSystem = new IdentifiableObjectPrevalentSystem(); 
-	 * Prevayler prevayler = PrevaylerFactory.createTransientPrevayler(prevalantSystem);
+	 * Prevayler prevayler = PrevaylerFactory.createTransientPrevayler(prevalantSystem); 
 	 * return prevayler;
 	 * 
-	 * @return a Prevayler instance configured as you desire.  See PrevaylerFactory.create methods for more info.
+	 * @return a Prevayler instance configured as you desire. See PrevaylerFactory.create methods for more info.
 	 */
 	protected abstract Class<Prevayler> getPrevayler();
 }
